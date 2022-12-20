ed94271251e6139f207321619efbed507d96a1e0
Ryan Shen <rjshen@uwaterloo.ca>
2022-12-20 18:29:17 -0500

    twitter scraper works

--- Twitter Scraper/twitter_api.py
+++ Twitter Scraper/twitter_api.py
@@ -1,27 +1,118 @@
-import tweepy
-import configparser 
-import os
+#Run this in command prompt on windows for best results
 
-#read configs API key with configparser
+import tweepy
+import configparser
+import pandas as pd
+
+# function to display data of each tweet
+def printtweetdata(n, ith_tweet):
+    print()
+    print(f"Tweet {n}:")
+    print(f"Username:{ith_tweet[0]}")
+    print(f"Description:{ith_tweet[1]}")
+    print(f"Location:{ith_tweet[2]}")
+    print(f"Following Count:{ith_tweet[3]}")
+    print(f"Follower Count:{ith_tweet[4]}")
+    print(f"Total Tweets:{ith_tweet[5]}")
+    print(f"Retweet Count:{ith_tweet[6]}")
+    print(f"Tweet Text:{ith_tweet[7]}")
+    print(f"Hashtags Used:{ith_tweet[8]}")
+
+# function to perform data extraction
+def scrape(words, date_since, numtweet):
+    # Creating DataFrame using pandas
+    db = pd.DataFrame(columns=['username',
+                               'description',
+                               'location',
+                               'following',
+                               'followers',
+                               'totaltweets',
+                               'retweetcount',
+                               'text',
+                               'hashtags'])
+                            
+    # We are using .Cursor() to search
+    # through twitter for the required tweets.
+    # The number of tweets can be
+    # restricted using .items(number of tweets)
+    tweets = tweepy.Cursor(api.search_tweets,
+                           words, lang="en",
+                           since_id=date_since,
+                           tweet_mode='extended').items(numtweet)
+
+    # .Cursor() returns an iterable object. Each item in
+    # the iterator has various attributes
+    # that you can access to
+    # get information about each tweet
+    list_tweets = [tweet for tweet in tweets]
+
+    # Counter to maintain Tweet Count
+    i = 1
+
+    # we will iterate over each tweet in the
+    # list for extracting information about each tweet
+    for tweet in list_tweets:
+        username = tweet.user.screen_name
+        description = tweet.user.description
+        location = tweet.user.location
+        following = tweet.user.friends_count
+        followers = tweet.user.followers_count
+        totaltweets = tweet.user.statuses_count
+        retweetcount = tweet.retweet_count
+        hashtags = tweet.entities['hashtags']
+
+        # Retweets can be distinguished by
+        # a retweeted_status attribute,
+        # in case it is an invalid reference,
+        # except block will be executed
+        try:
+            text = tweet.retweeted_status.full_text
+        except AttributeError:
+            text = tweet.full_text
+        hashtext = list()
+        for j in range(0, len(hashtags)):
+            hashtext.append(hashtags[j]['text'])
+
+        # Here we are appending all the
+        # extracted information in the DataFrame
+        ith_tweet = [username, description,
+                     location, following,
+                     followers, totaltweets,
+                     retweetcount, text, hashtext]
+        db.loc[len(db)] = ith_tweet
+
+        # Function call to print tweet data on screen
+        printtweetdata(i, ith_tweet)
+        i = i+1
+    filename = 'scraped_tweets.csv'
+
+    # we will save our database as a CSV file.
+    db.to_csv(filename)
+
+
+# read configs API key with configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-api_key = config['hello']['api_key']
+api_key = config['twitter']['api_key']
 api_key_secret = config['twitter']['api_key_secret']
 
 access_token = config['twitter']['access_token']
 access_token_secret = config['twitter']['access_token_secret']
 
-#authentication
-auth = tweepy.OAuthHandler(consumer_key = api_key, consumer_secret = api_key_secret)
+# authentication
+auth = tweepy.OAuthHandler(
+    consumer_key=api_key, consumer_secret=api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 
-choice = input("enter the hashtag: ")
+# Enter Hashtag and initial date
+print("Enter Twitter HashTag to search for")
+words = input()
+print("Enter Date since The Tweets are required in yyyy-mm--dd")
+date_since = input()
 
-tweets = tweepy.Cursor(api.search_tweets, q = choice).items(100)
-
-for tweet in tweets:
-    print(tweet.text)
-    print("\n")
+# number of tweets you want to extract in one run
+numtweet = 100
+scrape(words, date_since, numtweet)
+print('Scraping has completed!')
