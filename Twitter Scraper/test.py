fd97ee3684152c2ba0d9e8de81da47c210fd8806
Ryan Shen <rjshen@uwaterloo.ca>
2022-12-20 18:17:56 -0500

    tweepy authenticates

--- Twitter Scraper/test.py
+++
@@ -1,7 +0,0 @@
-import configparser 
-
-#read configs API key with configparser
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-api_key = config['hello']['api_key']
--- Twitter Scraper/twitter_api.py
+++ Twitter Scraper/twitter_api.py
@@ -12,4 +12,16 @@ api_key_secret = config['twitter']['api_key_secret']
 access_token = config['twitter']['access_token']
 access_token_secret = config['twitter']['access_token_secret']
 
-print(api_key)
+#authentication
+auth = tweepy.OAuthHandler(consumer_key = api_key, consumer_secret = api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+choice = input("enter the hashtag: ")
+
+tweets = tweepy.Cursor(api.search_tweets, q = choice).items(100)
+
+for tweet in tweets:
+    print(tweet.text)
+    print("\n")
