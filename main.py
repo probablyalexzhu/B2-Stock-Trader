6c230fbb6c1a06cc007d3078411ea79190e85dd0
Ryan Shen <rjshen@uwaterloo.ca>
2023-01-07 17:31:14 -0500

    goated stock picks

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from GoogleScraper.googlescraper import *
 from GoogleScraper.tickerparser import *
 from StockPicker.configexporter import *
 from StockPicker.portfolioPicker import *
-from NYTScraper.mainNYT import *
+from NYTScraper.MainNYT import *
 from NYTScraper.cohereSentiment import *
 
 # Should be run in this order: generate tickers, generate search scores, generate article text, generate nytscores, sum score, pick stocks, print portfolio
 def main():
     # print("running")
-    generateTickersByYear(2016)
-    generateSearchScores()
-    graph()
-    generateArticleText()
-    generateNYTScores()
-    sumScore()
+    # generateTickersByYear(2008)
+    # generateSearchScores()
+    # graph()
+    # generateArticleText()
+    # generateNYTScores()
+    # sumScore()
     pickStocks()
     printPortfolio()
 
